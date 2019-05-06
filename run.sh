05b3fdbb6086fde7936a3d919109fca99600b103
dayjaby <dayjaby@gmail.com>
2019-05-06 14:39:09 +0200

    added iris_fpv_cam as model with stream

--- run.sh
+++ run.sh
@@ -5,10 +5,12 @@ export PX4_HOME_LAT=53.5450421
 export PX4_HOME_LON=9.9957323
 export PX4_HOME_ALT=5
 export PX4_SIM_SPEED_FACTOR=1
-export model="iris"
-export PX4_SIM_MODEL="iris"
+# export model="iris"
+export HEADLESS=1
+# export PX4_SIM_MODEL="iris_fpv_cam"
+export PX4_SIM_MODEL="typhoon_h480"
 source "./Tools/setup_gazebo.bash" . ./build/px4_sitl_default
-gzserver --verbose "./Tools/sitl_gazebo/worlds/${model}.world" &                                                                                                                         
+gzserver --verbose "./Tools/sitl_gazebo/worlds/${PX4_SIM_MODEL}.world" &
 
 set +e
 export rootfs="./build/px4_sitl_default/tmp/rootfs"
