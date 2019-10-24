cdb87535185a599e175330e8caa4a64e82b61961
David Jablonski <dayjaby@gmail.com>
2019-10-24 13:31:15 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 export DONT_RUN=1
-export PX4_HOME_LAT=53.5450421
-export PX4_HOME_LON=9.9957323
+export PX4_HOME_LAT=53.5177986
+export PX4_HOME_LON=9.8757552
 export PX4_HOME_ALT=5
 export PX4_SIM_SPEED_FACTOR=1
-# export model="iris"
 export HEADLESS=1
-# export PX4_SIM_MODEL="iris_fpv_cam"
 export PX4_SIM_MODEL="typhoon_h480"
 source "./Tools/setup_gazebo.bash" . ./build/px4_sitl_default
 # gzserver --verbose "./Tools/sitl_gazebo/worlds/${PX4_SIM_MODEL}.world" &
