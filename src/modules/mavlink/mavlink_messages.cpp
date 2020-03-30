a8ccd4ddb30c1bd0d7956ae1faa00e97d8e1ddb0
David Jablonski <dayjaby@gmail.com>
2020-03-30 20:51:29 +0200

    added non templated base class

--- src/modules/mavlink/mavlink_messages.cpp
+++ src/modules/mavlink/mavlink_messages.cpp
@@ -1032,36 +1032,19 @@ public:
 	}
 };
 
-template <int N, typename Derived>
+const char* MSG_NAMES_SCALED_IMU[] = {
+	"SCALED_IMU",
+	"SCALED_IMU2",
+	"SCALED_IMU3"
+};
+
 class MavlinkStreamScaledIMUBase : public MavlinkStream
 {
-public:
-	const char *get_name() const override
-	{
-		return Derived::get_name_static();
-	}
-
-	uint16_t get_id() override
-	{
-		return Derived::get_id_static();
-	}
-
-	static MavlinkStream *new_instance(Mavlink *mavlink)
-	{
-		return new Derived(mavlink);
-	}
-
-private:
-	uORB::Subscription _raw_accel_sub{ORB_ID(sensor_accel_integrated), N};
-	uORB::Subscription _raw_gyro_sub{ORB_ID(sensor_gyro_integrated), N};
-	uORB::Subscription _raw_mag_sub{ORB_ID(sensor_mag), N};
-
-	// do not allow to copy this class
-	MavlinkStreamScaledIMUBase(MavlinkStreamScaledIMUBase &) = delete;
-	MavlinkStreamScaledIMUBase &operator = (const MavlinkStreamScaledIMUBase &) = delete;
-
 protected:
-	explicit MavlinkStreamScaledIMUBase(Mavlink *mavlink) : MavlinkStream(mavlink)
+	explicit MavlinkStreamScaledIMUBase(Mavlink *mavlink, int N) : MavlinkStream(mavlink),
+		_raw_accel_sub(ORB_ID(sensor_accel_integrated), N),
+		_raw_gyro_sub(ORB_ID(sensor_gyro_integrated), N),
+		_raw_mag_sub(ORB_ID(sensor_mag), N)
 	{}
 
 	bool send(const hrt_abstime t) override
@@ -1077,127 +1060,121 @@ protected:
 			sensor_mag_s sensor_mag{};
 			_raw_mag_sub.copy(&sensor_mag);
 
-			typename Derived::mav_msg_type msg{};
-
-			msg.time_boot_ms = sensor_accel.timestamp / 1000;
-
 			// Accelerometer in mG
 			const float accel_dt_inv = 1.e6f / (float)sensor_accel.dt;
 			const Vector3f accel = Vector3f{sensor_accel.delta_velocity} * accel_dt_inv * 1000.0f / CONSTANTS_ONE_G;
 
-			// Gyroscope in mrad/s
 			const float gyro_dt_inv = 1.e6f / (float)sensor_gyro.dt;
 			const Vector3f gyro = Vector3f{sensor_gyro.delta_angle} * gyro_dt_inv * 1000.0f;
 
-			msg.xacc = (int16_t)accel(0);
-			msg.yacc = (int16_t)accel(1);
-			msg.zacc = (int16_t)accel(2);
-			msg.xgyro = gyro(0);
-			msg.ygyro = gyro(1);
-			msg.zgyro = gyro(2);
-			msg.xmag = sensor_mag.x * 1000.0f; // Gauss -> MilliGauss
-			msg.ymag = sensor_mag.y * 1000.0f; // Gauss -> MilliGauss
-			msg.zmag = sensor_mag.z * 1000.0f; // Gauss -> MilliGauss
-			msg.temperature = sensor_mag.temperature;
-
-			Derived::send(_mavlink->get_channel(), &msg);
-
+			send_mavlink_message(sensor_accel.timestamp / 1000, accel, gyro, sensor_mag);
 			return true;
 		}
 
 		return false;
+	}
+	
+	virtual void send_mavlink_message(uint32_t time_boot_ms, const Vector3f& sensor_accel, const Vector3f& gyro, sensor_mag_s& sensor_mag) = 0;
+
+private:
+	uORB::Subscription _raw_accel_sub;
+	uORB::Subscription _raw_gyro_sub;
+	uORB::Subscription _raw_mag_sub;
+
+	// do not allow to copy this class
+	MavlinkStreamScaledIMUBase(MavlinkStreamScaledIMUBase &) = delete;
+	MavlinkStreamScaledIMUBase &operator = (const MavlinkStreamScaledIMUBase &) = delete;
+};
+
+template <int N, typename Derived, uint16_t MSG_ID_SCALED_IMU, unsigned MSG_ID_SCALED_IMU_LEN, typename MavlinkMsg>
+class MavlinkStreamScaledIMUTemplate : public MavlinkStreamScaledIMUBase
+{
+public:
+	const char *get_name() const override
+	{
+		return get_name_static();
+	}
+
+	static const char* get_name_static()
+	{
+		return MSG_NAMES_SCALED_IMU[N];
+	}
+
+	uint16_t get_id() override
+	{
+		return get_id_static();
+	}
+
+	static constexpr uint16_t get_id_static()
+	{
+		return MSG_ID_SCALED_IMU;
+	}
+
+	unsigned get_size() override
+	{
+		return MSG_ID_SCALED_IMU_LEN + MAVLINK_NUM_NON_PAYLOAD_BYTES;
+	}
+
+	static MavlinkStream *new_instance(Mavlink *mavlink)
+	{
+		return new Derived(mavlink);
+	}
+
+private:
+	typedef void (*SendMavlinkHandler)(mavlink_channel_t,  const MavlinkMsg*);
+	SendMavlinkHandler _send_mavlink;
+
+protected:
+	explicit MavlinkStreamScaledIMUTemplate(Mavlink *mavlink, SendMavlinkHandler handler) : MavlinkStreamScaledIMUBase(mavlink, N),
+		_send_mavlink(handler)
+	{}
+
+	void send_mavlink_message(uint32_t time_boot_ms, const Vector3f& accel, const Vector3f& gyro, sensor_mag_s& sensor_mag)
+	{
+		MavlinkMsg msg{};
+		msg.time_boot_ms = time_boot_ms;
+		msg.xacc = (int16_t)accel(0);
+		msg.yacc = (int16_t)accel(1);
+		msg.zacc = (int16_t)accel(2);
+		msg.xgyro = gyro(0);
+		msg.ygyro = gyro(1);
+		msg.zgyro = gyro(2);
+		msg.xmag = sensor_mag.x * 1000.0f; // Gauss -> MilliGauss
+		msg.ymag = sensor_mag.y * 1000.0f; // Gauss -> MilliGauss
+		msg.zmag = sensor_mag.z * 1000.0f; // Gauss -> MilliGauss
+		msg.temperature = sensor_mag.temperature;
+
+		_send_mavlink(_mavlink->get_channel(), &msg);
 	}
 };
 
 template <int N> struct MavlinkStreamScaledIMU {};
 
 template <>
-class MavlinkStreamScaledIMU<0> : public MavlinkStreamScaledIMUBase<0, MavlinkStreamScaledIMU<0> >
+class MavlinkStreamScaledIMU<0> : public MavlinkStreamScaledIMUTemplate<0, MavlinkStreamScaledIMU<0>, MAVLINK_MSG_ID_SCALED_IMU, MAVLINK_MSG_ID_SCALED_IMU_LEN, mavlink_scaled_imu_t>
 {
 public:
-	typedef MavlinkStreamScaledIMUBase<0, MavlinkStreamScaledIMU<0> > Base;
-	typedef mavlink_scaled_imu_t mav_msg_type;
+	typedef MavlinkStreamScaledIMUTemplate<0, MavlinkStreamScaledIMU<0>, MAVLINK_MSG_ID_SCALED_IMU, MAVLINK_MSG_ID_SCALED_IMU_LEN, mavlink_scaled_imu_t> Base;
 
-	explicit MavlinkStreamScaledIMU(Mavlink *mavlink) : Base(mavlink) {}
-
-	static void send(mavlink_channel_t channel, const MavlinkStreamScaledIMU<0>::mav_msg_type *msg)
-	{
-		mavlink_msg_scaled_imu_send_struct(channel, msg);
-	}
-
-	static constexpr const char *get_name_static()
-	{
-		return "SCALED_IMU";
-	}
-
-	static constexpr uint16_t get_id_static()
-	{
-		return MAVLINK_MSG_ID_SCALED_IMU;
-	}
-
-	unsigned get_size() override
-	{
-		return MAVLINK_MSG_ID_SCALED_IMU_LEN + MAVLINK_NUM_NON_PAYLOAD_BYTES;
-	}
+	explicit MavlinkStreamScaledIMU(Mavlink *mavlink) : Base(mavlink, mavlink_msg_scaled_imu_send_struct) {}
 };
 
 template <>
-class MavlinkStreamScaledIMU<1> : public MavlinkStreamScaledIMUBase<1, MavlinkStreamScaledIMU<1> >
+class MavlinkStreamScaledIMU<1> : public MavlinkStreamScaledIMUTemplate<1, MavlinkStreamScaledIMU<1>, MAVLINK_MSG_ID_SCALED_IMU2, MAVLINK_MSG_ID_SCALED_IMU2_LEN, mavlink_scaled_imu2_t>
 {
 public:
-	typedef MavlinkStreamScaledIMUBase<1, MavlinkStreamScaledIMU<1> > Base;
-	typedef mavlink_scaled_imu2_t mav_msg_type;
+	typedef MavlinkStreamScaledIMUTemplate<1, MavlinkStreamScaledIMU<1>, MAVLINK_MSG_ID_SCALED_IMU2, MAVLINK_MSG_ID_SCALED_IMU2_LEN, mavlink_scaled_imu2_t> Base;
 
-	explicit MavlinkStreamScaledIMU(Mavlink *mavlink) : Base(mavlink) {}
-
-	static void send(mavlink_channel_t channel, const MavlinkStreamScaledIMU<1>::mav_msg_type *msg)
-	{
-		mavlink_msg_scaled_imu2_send_struct(channel, msg);
-	}
-
-	static const char *get_name_static()
-	{
-		return "SCALED_IMU2";
-	}
-
-	static uint16_t get_id_static()
-	{
-		return MAVLINK_MSG_ID_SCALED_IMU2;
-	}
-
-	unsigned get_size() override
-	{
-		return MAVLINK_MSG_ID_SCALED_IMU2_LEN + MAVLINK_NUM_NON_PAYLOAD_BYTES;
-	}
+	explicit MavlinkStreamScaledIMU(Mavlink *mavlink) : Base(mavlink, mavlink_msg_scaled_imu2_send_struct) {}
 };
 
 template <>
-class MavlinkStreamScaledIMU<2> : public MavlinkStreamScaledIMUBase<2, MavlinkStreamScaledIMU<2> >
+class MavlinkStreamScaledIMU<2> : public MavlinkStreamScaledIMUTemplate<2, MavlinkStreamScaledIMU<2>, MAVLINK_MSG_ID_SCALED_IMU3, MAVLINK_MSG_ID_SCALED_IMU3_LEN, mavlink_scaled_imu3_t>
 {
 public:
-	typedef MavlinkStreamScaledIMUBase<2, MavlinkStreamScaledIMU<2> > Base;
-	typedef mavlink_scaled_imu3_t mav_msg_type;
+	typedef MavlinkStreamScaledIMUTemplate<2, MavlinkStreamScaledIMU<2>, MAVLINK_MSG_ID_SCALED_IMU3, MAVLINK_MSG_ID_SCALED_IMU3_LEN, mavlink_scaled_imu3_t> Base;
 
-	explicit MavlinkStreamScaledIMU(Mavlink *mavlink) : Base(mavlink) {}
-
-	static void send(mavlink_channel_t channel, const MavlinkStreamScaledIMU<2>::mav_msg_type *msg)
-	{
-		mavlink_msg_scaled_imu3_send_struct(channel, msg);
-	}
-	static const char *get_name_static()
-	{
-		return "SCALED_IMU3";
-	}
-
-	static constexpr uint16_t get_id_static()
-	{
-		return MAVLINK_MSG_ID_SCALED_IMU3;
-	}
-
-	unsigned get_size() override
-	{
-		return MAVLINK_MSG_ID_SCALED_IMU3_LEN + MAVLINK_NUM_NON_PAYLOAD_BYTES;
-	}
+	explicit MavlinkStreamScaledIMU(Mavlink *mavlink) : Base(mavlink, mavlink_msg_scaled_imu3_send_struct) {}
 };
 
 class MavlinkStreamAttitude : public MavlinkStream
